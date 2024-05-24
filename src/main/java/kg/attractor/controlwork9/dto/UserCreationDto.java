84957b17db5e9feabe8d67256f42e7c15fdf1a23
DesBasito <116246777+DesBasito@users.noreply.github.com>
2024-05-24 20:08:30 +0600

    added localization

--- src/main/java/kg/attractor/controlwork9/dto/UserCreationDto.java
+++ src/main/java/kg/attractor/controlwork9/dto/UserCreationDto.java
@@ -2,6 +2,8 @@ package kg.attractor.controlwork9.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,11 +16,17 @@ import lombok.NoArgsConstructor;
 public class UserCreationDto {
     @Email @NotBlank
     private String email;
+
     @NotBlank
     private String name;
+
     @NotBlank
     private String surname;
+
     @NotBlank
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).+$", message = "{register.valid.password}")
     private String password;
+
+    @NotNull @NotBlank
     private String role;
 }
